6c6083ae423e2d90e94bf6f7d8a183dd2432961f
Mingfeng-Wang <1164291574@qq.com>
2021-10-13 17:56:31 +0800

    more baseline cfg

--- projects/configs/_base_/datasets/coco_detection_16pergpu_v100_featurize.py
+++ projects/configs/_base_/datasets/coco_detection_16pergpu_v100_featurize.py
@@ -30,7 +30,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=16,
-    workers_per_gpu=16,
+    workers_per_gpu=32,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
